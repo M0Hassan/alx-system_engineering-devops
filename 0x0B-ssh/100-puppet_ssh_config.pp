162e51d8df7b9c204aea58a3edfe1ce14c3d2d65
root <aminmohamedadan@gmail.com>
2022-01-05 06:58:01 +0000

    script configures the configuration file not to require password authentication

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 # sets up a client SSH configuration file so that we can connect to a server without using a password
 include stdlib
 file_line { 'Declare identity file':
+  ensure  => present, 
   path    => '/etc/ssh/ssh_config',
-  line    => '    IdentityFile ~/.ssh/holberton',
-  replace => true,
+  line    => 'IdentityFile ~/.ssh/holberton',
 }
 
 file_line { 'Turn off passwd auth':
+  ensure  => present,	
   path    => '/etc/ssh/ssh_config',
-  line    => '    PasswordAuthentication no',
-  replace => true,
+  line    => 'PasswordAuthentication no',
 }
